b15a6cbd90b6338d5371803043b2279ffb756f23
candy06 <loic.rose1995@gmail.com>
2018-05-11 22:31:22 +0200

    Implementation of scatter (works for 2 processors, but not more...)

--- src/main.c
+++ src/main.c
@@ -34,8 +34,17 @@
 ********************************************************************************
 */
 
-int number_of_processes, current_processID, nextNode, previousNode, lastNode;
+int number_of_processes,
+    current_processID,
+    nextNode,
+    previousNode,
+    lastNode,
+    sizeOfSubmatrix;
+
 int vector[2];
+int matrix[16];
+int * tmp = NULL;
+int * submatrix = NULL;
 
 
 /*
@@ -64,6 +73,30 @@ void displayVector(int * vector, int size)
 
 /*
 ********************************************************************************
+*                                   displayMatrix()
+*
+*
+* Description : display a matrix
+* Arguments : array of integers (elements of the matrix) and integer (size)
+* Return : void
+********************************************************************************
+*/
+
+void displayMatrix(int * matrix, int size)
+{
+  printf("Matrix: |");
+  for (int i = 0 ; i < size ; i++) {
+    if (i != size - 1) {
+      printf(" %d,", matrix[i]);
+    } else {
+      printf(" %d |\n", matrix[i]);
+    }
+  }
+}
+
+
+/*
+********************************************************************************
 *                                   init()
 *
 *
@@ -78,6 +111,9 @@ void init()
   nextNode = (current_processID + 1) % number_of_processes;
   previousNode = (current_processID - 1 + number_of_processes) % number_of_processes;
   lastNode = (ROOT_PROCESS_ID + number_of_processes - 1) % number_of_processes;
+  sizeOfSubmatrix = 16 / number_of_processes;
+  submatrix = malloc(sizeOfSubmatrix * sizeof(int));
+  tmp = malloc(sizeOfSubmatrix * sizeof(int));
 }
 
 
@@ -107,6 +143,37 @@ void broadcast(int * data, int size_of_the_data)
 
 /*
 ********************************************************************************
+*                                   scatter()
+*
+*
+* Description : scatter the data to all the nodes
+* Arguments : the data and its size + the reception data and its size
+* Return : void
+********************************************************************************
+*/
+
+void scatter(int * dataSource, int size_of_the_dataSource, int * dataDestination, int size_of_the_dataDestination)
+{
+
+  if (current_processID == ROOT_PROCESS_ID) {
+    for (int i = size_of_the_dataDestination ; i < size_of_the_dataSource ; i++) {
+      MPI_Send(&dataSource[i], size_of_the_dataDestination, MPI_INT, nextNode, 0, MPI_COMM_WORLD);
+    }
+    submatrix = &dataSource[current_processID];
+  } else {
+    MPI_Recv(dataDestination, size_of_the_dataDestination, MPI_INT, previousNode, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    for (int i = 0 ; i < (ROOT_PROCESS_ID - 1 - current_processID + number_of_processes) % number_of_processes ; i++) {
+      MPI_Send(dataDestination, size_of_the_dataDestination, MPI_INT, nextNode, 0, MPI_COMM_WORLD);
+      MPI_Recv(dataDestination, size_of_the_dataDestination, MPI_INT, previousNode, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    //submatrix = tmp;
+  }
+
+}
+
+
+/*
+********************************************************************************
 *                                   main()
 *
 *
@@ -127,13 +194,37 @@ int main(int argc, char const *argv[])
   init();
 
   if (current_processID == ROOT_PROCESS_ID) {
+    // Initialize the vector
     vector[0] = 5;
     vector[1] = 12;
+    // Initialize the matrix
+    matrix[0] = 1;
+    matrix[1] = 10;
+    matrix[2] = 2;
+    matrix[3] = 20;
+
+    matrix[4] = 3;
+    matrix[5] = 30;
+    matrix[6] = 4;
+    matrix[7] = 40;
+    matrix[8] = 5;
+
+    matrix[9] = 50;
+    matrix[10] = 6;
+    matrix[11] = 60;
+    matrix[12] = 7;
+    matrix[13] = 70;
+    matrix[14] = 8;
+    matrix[15] = 80;
+    displayMatrix(matrix, 16);
   }
 
-  broadcast(vector, 2);
+  //broadcast(vector, 2);
+  scatter(matrix, 16, submatrix, sizeOfSubmatrix);
+  // displayVector(vector, 2);
 
-  displayVector(vector, 2);
+  displayMatrix(submatrix, sizeOfSubmatrix);
+  // printf("Taille de chaque sous-matrices = %d\n", sizeOfSubmatrix);
   // printf("ProcessID: %d | nextNode = %d - previousNode = %d - lastNode = %d\n", current_processID, nextNode, previousNode, lastNode);
 
   MPI_Finalize();
